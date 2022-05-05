f0096dfc7a89be57483607a2daab442c58c675b4
Gerard Siles <gerardsiles@gmail.com>
2022-05-05 23:26:02 +0000

    views implementadas, route de usuarios

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| Aqui se generan las rutas de la aplicacion.
+| Las rutas llaman al controlador, que cargan la vista con la funcion.
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Rutas de estudiantes */
+route::get('/users', [UsersController::class, 'renderIndex']);
+route::get('/users/login', [UsersController::class, 'renderLogin']);
+route::post('/users/login', [UsersController::class, 'login']);
+route::get('/users/email', [UsersController::class, 'renderEmail']);
+route::patch('/users/email', [UsersController::class, 'editEmail']);
+route::get('/users/password', [UsersController::class, 'renderPassword']);
+route::patch('/users/password', [UsersController::class, 'editPassword']);
+route::get('/users/register', [UsersController::class, 'renderRegister']);
+route::post('/users/register', [UsersController::class, 'register']);
+route::get('/users/schedule', [UsersController::class, 'renderSchedule']);
+route::get('/users/username', [UsersController::class, 'renderUsername']);
+route::patch('/users/username', [UsersController::class, 'editUsername']);
+
+/* rutas de admin */
+
+
